21198e621fbefd638cca15022f090818ba9b3f0a
Lodewyk-Pretorius <lodewyk.pretorius1@gmail.com>
2026-04-23 14:46:46 +0200

    As good as I could get it
    Changed names, added minimal UI code, but the details are done

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,20 +1,92 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class frmTicketManagement : Form
     {
-        public Form1()
+        public frmTicketManagement()
         {
             InitializeComponent();
+            SetupSimpleUI();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // -------------------------------
+        // SIMPLE UI ENHANCEMENTS
+        // -------------------------------
+        private void SetupSimpleUI()
         {
+            // Button hover effects
+            AddHover(btnCalculate);
+            AddHover(btnClear);
+            AddHover(btnExit);
 
+            // Textbox focus highlight
+            AddFocus(txtName);
+            AddFocus(txtAge);
+            AddFocus(txtDistance);
+
+            // Only allow numbers for Age & Distance
+            txtAge.KeyPress += OnlyNumbers;
+            txtDistance.KeyPress += OnlyNumbers;
+
+            // Cursor polish
+            btnCalculate.Cursor = Cursors.Hand;
+            btnClear.Cursor = Cursors.Hand;
+            btnExit.Cursor = Cursors.Hand;
         }
 
+        // -------------------------------
+        // BUTTON HOVER EFFECT
+        // -------------------------------
+        private void AddHover(Button btn)
+        {
+            Color original = btn.BackColor;
+
+            btn.MouseEnter += (s, e) =>
+            {
+                btn.BackColor = ControlPaint.Dark(original);
+            };
+
+            btn.MouseLeave += (s, e) =>
+            {
+                btn.BackColor = original;
+            };
+        }
+
+        // -------------------------------
+        // TEXTBOX FOCUS EFFECT
+        // -------------------------------
+        private void AddFocus(TextBox txt)
+        {
+            txt.Enter += (s, e) =>
+            {
+                txt.BackColor = Color.LightYellow;
+            };
+
+            txt.Leave += (s, e) =>
+            {
+                txt.BackColor = Color.White;
+            };
+        }
+
+        // -------------------------------
+        // ONLY NUMBERS INPUT
+        // -------------------------------
+        private void OnlyNumbers(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Designer references this click handler (lblName.Click += rrn;)
+        // Provide an empty implementation so the Designer can load.
         private void rrn(object sender, EventArgs e)
         {
-
+            // Intentionally empty - kept to satisfy Designer event hookup.
         }
     }
 }
